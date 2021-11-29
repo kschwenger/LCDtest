d877476c78b90f3316b56202a73ca035a74305e1
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-29 20:37:55 +0000

    sdfsadf

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def write(x, y, str):
 if __name__ == '__main__':
   init(0x27, 1)
 
-  line1 = 'HIT DETECTED'
+  line1 = 'HIT DETECTED!'
   line2 = ' '
-  write(int((16-len(line1))/2), 0, line1)
+  write(1, 0, line1)
   write(int((16-len(line2))/2), 1, line2)
